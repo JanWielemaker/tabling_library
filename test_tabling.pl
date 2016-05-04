891cc79272de260875aacc4c4fc61e33c357cadf
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-05-04 11:16:21 +0200

    Ported all tabling tests to PlUnit

--- test_tabling.pl
+++ test_tabling.pl
@@ -5,14 +5,30 @@
 :- use_module(testlib).
 :- use_module(testlib2).
 :- use_module(library(plunit)).
+:- use_module(library(debug)).
 
 test_tabling :-
 	run_tests([ tabling_ex1,
 		    tabling_ex2,
 		    tabling_ex3,
 		    tabling_ex4,
+		    tabling_ex5,
 		    tabling_ex6,
-		    tabling_ex7
+		    tabling_ex7,
+		    tabling_ex8,
+		    tabling_ex9a,
+		    tabling_ex9b,
+		    tabling_ex9c,
+		    tabling_ex9d,
+		    tabling_ex9e,
+		    tabling_ex10,
+		    tabling_ex11,
+		    tabling_ex12,
+		    tabling_ex13,
+		    tabling_ex14,
+		    tabling_ex15,
+		    tabling_ex16,
+		    tabling_ex17
 		  ]).
 
 		 /*******************************
@@ -199,6 +215,38 @@ test(ex4) :-
 		 *******************************/
 
 :- begin_tests(tabling_ex5, [cleanup(abolish_all_tables)]).
+% Variation on example 1 but with a  cycle. This is an important example
+% for tabling.
+
+expected_variants([f(1,_),f(2,_),f(3,_),f(_,_)]).
+expected_answers_for_variant(f(_,_),L) :-
+  findall(f(X,Y),(between(1,3,X),between(1,3,Y)),L).
+expected_answers_for_variant(f(1,_),L) :-
+  findall(f(1,X),between(1,3,X),L).
+expected_answers_for_variant(f(2,_),L) :-
+  findall(f(2,X),between(1,3,X),L).
+expected_answers_for_variant(f(3,_),L) :-
+  findall(f(3,X),between(1,3,X),L).
+
+% The answers we expect for example 5,   returned as a list with entries
+% of the form X-Y. The order does not matter. We expect nine answers.
+f_expected_answers([1-1,1-2,1-3,2-1,2-2,2-3,3-1,3-2,3-3]).
+
+:- table f/2.
+
+f(X,Y) :- before, f(X,Z), between, f(Z,Y).
+f(X,Y) :- e2(X,Y).
+
+e2(1,2).
+e2(2,3).
+e2(3,1).
+
+test(ex5) :-
+	findall(_,f(_,_),_),
+	compare_real_expected_answers(f,2,f_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
 :- end_tests(tabling_ex5).
 
 
@@ -328,7 +376,529 @@ test(ex7) :-
 		 *******************************/
 
 :- begin_tests(tabling_ex8, [cleanup(abolish_all_tables)]).
+% Example specifically designed  to  test  whether   we  can  get  a new
+% continuation in the last iteration of  run_contins. This is indeed the
+% case. Thus in run_contins we  also  need   to  look  at  the number of
+% continuations to determine whether we can   stop.  However we had some
+% questions: we might need to look  whether   the  new continuation is a
+% variant of a continuation that is   already present (to avoid infinite
+% loops).
+%
+% j(a,b).
+% j(X,Y) :- j(_,_), j(Y,X).
+
+expected_variants([j(_,_)]).
+expected_answers_for_variant(j(_,_),[j(a,b),j(b,a)]).
+j_expected_answers([a-b,b-a]).
+
+:- table j/2.
+
+j(a,b) :-
+	feedback('j 1: give answer'). % TODO: check why we don*t have to save this answer here explicitly.
+j(X,Y) :-
+	feedback('j 2: before'),
+	j(_,_),
+	feedback('j 2: between'),
+	j(Y,X),
+	feedback('j 2: after').
+
+test(ex8) :-
+	compare_real_expected_answers(j,2,j_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
 :- end_tests(tabling_ex8).
+
+
+		 /*******************************
+		 *           EXAMPLE 9A		*
+		 *******************************/
+
+:- begin_tests(tabling_ex9a, [cleanup(abolish_all_tables)]).
+% The reachability predicate can be written in several variants.
+% Example 1: a
+% p(X,Y) :- p(X,Z), p(Z,Y).
+% p(X,Y) :- e(X,Y).
+%
+% Example 9a:
+% p(X,Y) :- p(X,Z), e(Z,Y).
+% p(X,Y) :- e(X,Y).
+%
+% Example 9b:
+% p(X,Y) :- e(X,Z), p(Z,Y).
+% p(X,Y) :- e(X,Y).
+%
+% Example 9c:
+% p(X,Y) :- e(X,Y).
+% p(X,Y) :- p(X,Z), p(Z,Y).
+%
+% Example 9d:
+% p(X,Y) :- e(X,Y).
+% p(X,Y) :- p(X,Z), e(Z,Y).
+%
+% Example 9e:
+% p(X,Y) :- e(X,Y).
+% p(X,Y) :- e(X,Z), p(Z,Y).
+
+expected_variants([a(_,_)]).
+expected_answers_for_variant(a(_,_),[a(1,2),a(2,3),a(1,3)]).
+
+% The answers we expect for reachability   variants,  returned as a list
+% with entries of the form X-Y. The order does not matter.
+
+reachability_expected_answers([1-2,2-3,1-3]).
+
+:- table a/2.
+
+a(X,Y) :-
+	feedback('before'),
+	a(X,Z),
+	feedback('between: prove ~w~n',[e(Z,Y)]),
+	e(Z,Y).
+a(X,Y) :-
+	e(X,Y).
+
+% Test facts for examples 1 and 2.
+e(1,2).
+e(2,3).
+
+test(ex9a) :-
+	compare_real_expected_answers(a,2,reachability_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+:- end_tests(tabling_ex9a).
+
+
+		 /*******************************
+		 *           EXAMPLE 9B		*
+		 *******************************/
+
+:- begin_tests(tabling_ex9b, [cleanup(abolish_all_tables)]).
+expected_variants([a(3,_),a(2,_),a(_,_)]).
+% Note: a(3,_) is an empty table, but it is there...
+expected_answers_for_variant(a(_,_),[a(1,2),a(2,3),a(1,3)]).
+expected_answers_for_variant(a(3,_),[]).
+expected_answers_for_variant(a(2,_),[a(2,3)]).
+reachability_expected_answers([1-2,2-3,1-3]).
+
+:- table a/2.
+
+a(X,Y) :-
+	feedback('before'),
+	e(X,Z),
+	feedback('between: prove ~w~n',[a(Z,Y)]),
+	a(Z,Y).
+a(X,Y) :-
+	e(X,Y).
+
+e(1,2).
+e(2,3).
+
+test(ex9b) :-
+	compare_real_expected_answers(a,2,reachability_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex9b).
+
+
+
+		 /*******************************
+		 *           EXAMPLE 9C		*
+		 *******************************/
+
+:- begin_tests(tabling_ex9c, [cleanup(abolish_all_tables)]).
+expected_variants([a(2,_),a(3,_),a(_,_)]).
+% Note: a(3,_) is an empty table, but it is there...
+expected_answers_for_variant(a(_,_),[a(1,2),a(2,3),a(1,3)]).
+expected_answers_for_variant(a(2,_),[a(2,3)]).
+expected_answers_for_variant(a(3,_),[]).
+
+reachability_expected_answers([1-2,2-3,1-3]).
+
+:- table a/2.
+
+a(X,Y) :- e(X,Y).
+a(X,Y) :- feedback('before'), a(X,Z), feedback('between'),  a(Z,Y).
+
+e(1,2).
+e(2,3).
+
+test(ex9c) :-
+	compare_real_expected_answers(a,2,reachability_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex9c).
+
+
+		 /*******************************
+		 *           EXAMPLE 9D		*
+		 *******************************/
+
+:- begin_tests(tabling_ex9d, [cleanup(abolish_all_tables)]).
+expected_variants([a(_,_)]).
+expected_answers_for_variant(a(_,_),[a(1,2),a(2,3),a(1,3)]).
+
+reachability_expected_answers([1-2,2-3,1-3]).
+
+:- table a/2.
+
+a(X,Y) :- e(X,Y).
+a(X,Y) :- feedback('before'), a(X,Z), feedback('between'),  e(Z,Y).
+
+e(1,2).
+e(2,3).
+
+test(ex9d) :-
+	compare_real_expected_answers(a,2,reachability_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex9d).
+
+
+		 /*******************************
+		 *           EXAMPLE 9E		*
+		 *******************************/
+
+:- begin_tests(tabling_ex9e, [cleanup(abolish_all_tables)]).
+expected_variants([a(3,_),a(2,_),a(_,_)]).
+% Note: a(3,_) is an empty table, but it is there
+expected_answers_for_variant(a(_,_),[a(1,2),a(2,3),a(1,3)]).
+expected_answers_for_variant(a(3,_),[]).
+expected_answers_for_variant(a(2,_),[a(2,3)]).
+
+reachability_expected_answers([1-2,2-3,1-3]).
+
+:- table a/2.
+
+a(X,Y) :- e(X,Y).
+a(X,Y) :- feedback('before'), e(X,Z), feedback('between'),  a(Z,Y).
+
+e(1,2).
+e(2,3).
+
+test(ex9d) :-
+	compare_real_expected_answers(a,2,reachability_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex9e).
+
+
+		 /*******************************
+		 *           EXAMPLE 10		*
+		 *******************************/
+
+:- begin_tests(tabling_ex10, [cleanup(abolish_all_tables)]).
+%d(X) <- e(Y), Y < 5, X is Y + 1.  % Will never run, because e doesn't have any facts
+%d(X) <- d(Y), Y < 20, X is Y + 5. % Should run
+%d(0).
+%e(X) <- d(Y), Y < 5, X is Y + 1.
+%% No facts for e
+
+expected_variants([d(_),e(_)]).
+expected_answers_for_variant(d(_),[d(0),d(2),d(4),d(5),d(7),d(9),
+				   d(10),d(12),d(14),d(15),d(17),d(19),
+				   d(20),d(22),d(24)]).
+expected_answers_for_variant(e(_),[e(5),e(3),e(1)]).
+
+d_expected_answers([0,2,4,5,7,9,10,12,14,15,17,19,20,22,24]).
+e_expected_answers([5,3,1]).
+
+:- table d/1, e/1.
+
+d(X) :- e(Y), Y < 5, X is Y + 1.  % Will never run, because e doesn't have any facts (initially)
+d(X) :- d(Y), Y < 20, X is Y + 5. % Should run
+d(0).
+e(X) :- d(Y), Y < 5, X is Y + 1.
+% No facts for e
+
+test(ex10) :-
+	compare_real_expected_answers(d,1,d_expected_answers),
+	compare_real_expected_answers(e,1,e_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex10).
+
+
+		 /*******************************
+		 *           EXAMPLE 11		*
+		 *******************************/
+
+:- begin_tests(tabling_ex11, [cleanup(abolish_all_tables)]).
+% Expected answers:
+% d(0), d(2), d(4)
+% e(1), e(3), e(5)
+
+expected_variants([d(_),e(_)]).
+expected_answers_for_variant(d(_),[d(0),d(2),d(4)]).
+expected_answers_for_variant(e(_),[e(1),e(3),e(5)]).
+
+d_expected_answers([0,2,4]).
+e_expected_answers([1,3,5]).
+
+:- table d/1, e/1.
+
+d(X) :- e(Y), Y < 5, X is Y + 1.
+d(0).
+e(X) :- d(Y), Y < 5, X is Y + 1.
+% No facts for e
+
+test(ex11) :-
+	compare_real_expected_answers(d,1,d_expected_answers),
+	compare_real_expected_answers(e,1,e_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex11).
+
+
+		 /*******************************
+		 *           EXAMPLE 12		*
+		 *******************************/
+
+:- begin_tests(tabling_ex12, [cleanup(abolish_all_tables)]).
+expected_variants([f(4),f(3),f(2),f(0),f(1),e(_),d(_),f(_)]).
+% Note: f(4) and f(0) are empty tables, but they are there...
+expected_answers_for_variant(d(_),L) :-
+  findall(d(X),between(0,4,X),L).
+expected_answers_for_variant(f(4),[]).
+expected_answers_for_variant(f(3),[f(3)]).
+expected_answers_for_variant(f(2),[f(2)]).
+expected_answers_for_variant(f(0),[]).
+expected_answers_for_variant(f(1),[f(1)]).
+expected_answers_for_variant(e(_),L) :-
+  findall(e(X),between(0,3,X),L).
+expected_answers_for_variant(f(_),L) :-	% JW: Added
+  findall(f(X),between(1,3,X),L).
+
+d_expected_answers([0,1,2,3,4]).
+e_expected_answers([0,1,2,3]).
+f_expected_answers([1,2,3]).
+
+:- table d/1, e/1, f/1.
+
+% Something like:
+d(X) :- e(Y), feedback('d_aux: after e(Y): ~w\n',[e(Y)]), X is Y + 1, X < 5.
+d(0).
+% Number of predicates involved in mutual recursion will increase at runtime
+e(X) :- d(X), f(X),
+	feedback('e_aux: at end of clause; head is now ~w\n',[e_aux(X)]).
+e(0) :- feedback('using fact e(0)\n',[]).
+f(X) :- e(Y), X is Y + 1, X < 4.
+
+test(ex12) :-
+	compare_real_expected_answers(d,1,d_expected_answers),
+	compare_real_expected_answers(e,1,e_expected_answers),
+	compare_real_expected_answers(f,1,f_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex12).
+
+		 /*******************************
+		 *           EXAMPLE 13		*
+		 *******************************/
+
+:- begin_tests(tabling_ex13, [cleanup(abolish_all_tables)]).
+expected_variants([p(2,_),p(1,_),p(_,_)]).
+expected_answers_for_variant(p(_,_),L) :-
+  findall(p(X,Y),(between(1,2,X),between(1,2,Y)),L).
+expected_answers_for_variant(p(2,_),L) :-
+  findall(p(2,X),between(1,2,X),L).
+expected_answers_for_variant(p(1,_),L) :-
+  findall(p(1,X),between(1,2,X),L).
+
+% The answers we expect for example 13, returned as a list with entries of the form X-Y. The order does not matter.
+% We expect four answers.
+p_expected_answers([1-2,2-1,1-1,2-2]).
+
+:- table p/2.
+
+p(X,Y) :-
+	feedback('before'), p(X,Z), feedback('between'), p(Z,Y).
+p(X,Y) :- e2(X,Y).
+
+e2(1,2).
+e2(2,1).
+
+test(ex12) :-
+	compare_real_expected_answers(p,2,p_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex13).
+
+
+		 /*******************************
+		 *           EXAMPLE 14		*
+		 *******************************/
+
+:- begin_tests(tabling_ex14, [cleanup(abolish_all_tables)]).
+% Simpler example than example12.pl, but the number of predicates involved in mutual recursion will also increase at runtime.
+
+expected_variants([p(3,_),p(2,_),q(2,_),q(3,_),p(_,_),q(_,_)]).
+% Note: p(3,_) and q(3,_) are empty tables, but they are there.
+expected_answers_for_variant(p(_,_),[p(1,2),p(2,3),p(1,3)]).
+expected_answers_for_variant(p(3,_),[]).
+expected_answers_for_variant(p(2,_),[p(2,3)]).
+expected_answers_for_variant(q(_,_),[q(1,2),q(1,3),q(2,3)]).
+expected_answers_for_variant(q(2,_),[q(2,3)]).
+expected_answers_for_variant(q(3,_),[]).
+
+p_expected_answers([1-2,2-3,1-3]).
+q_expected_answers([1-2,1-3,2-3]).	% JW: added
+
+go :-
+  once(p(_X,_Y)).
+
+:- table p/2, q/2.
+
+p(X,Y) :- p(X,Z), q(Z,Y).
+p(X,Y) :- e(X,Y).
+q(X,Y) :- p(X,Y).
+
+e(1,2).
+e(2,3).
+
+test(ex14) :-
+	compare_real_expected_answers(p,2,p_expected_answers),
+	compare_real_expected_answers(q,2,q_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex14).
+
+
+		 /*******************************
+		 *           EXAMPLE 15		*
+		 *******************************/
+
+:- begin_tests(tabling_ex15, [cleanup(abolish_all_tables)]).
+% Example designed to test whether the true   in fresh status works fine
+% in case only a continuation is saved. See issue 55.
+
+expected_variants([d(_),e(_)]).
+expected_answers_for_variant(d(_),L) :-
+  findall(d(X),between(0,5,X),L).
+expected_answers_for_variant(e(_),L) :-
+  findall(e(X),between(0,5,X),L).
+
+d_expected_answers([0,1,2,3,4,5]).
+e_expected_answers([0,1,2,3,4,5]).
+
+:- table d/1, e/1.
+
+d(X) :- e(Y), Y < 5, X is Y + 1.
+d(0).
+e(X) :- d(Y), e(_), Y < 5, X is Y + 1.
+e(0).
+
+test(ex15) :-
+	compare_real_expected_answers(d,1,d_expected_answers),
+	compare_real_expected_answers(e,1,e_expected_answers),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex15).
+
+
+		 /*******************************
+		 *           EXAMPLE 16		*
+		 *******************************/
+
+:- begin_tests(tabling_ex16, [cleanup(abolish_all_tables)]).
+% Expected outcome: there should not be a crash because d was already complete
+
+expected_variants([d(_),e(_),f(_),g(_)]).
+expected_answers_for_variant(d(_),L) :-
+  findall(d(X),between(0,5,X),L).
+expected_answers_for_variant(e(_),L) :-
+  findall(e(X),between(0,5,X),L).
+expected_answers_for_variant(f(_),L) :-
+  findall(f(X),between(0,5,X),L).
+expected_answers_for_variant(g(_),L) :-
+  findall(g(X),between(0,5,X),L).
+
+% Two mutually recursive predicates:
+% d(X) :- e(Y), Y < 5, X is Y + 1.
+% d(0).
+%
+% e(X) :- d(Y), Y < 5, X is Y + 1.
+% e(0).
+
+:- table d/1, e/1, f/1, g/1.
+
+d(X) :-
+	feedback('d_aux: before calling e(Y)'),
+	e(Y),
+	feedback('d_aux: after calling e(Y)'),
+	feedback('d_aux: Y is ~w~n',[Y]),
+	Y < 5,
+	feedback('d_aux: Y < 5 OK\n',[]),
+	(   X is Y + 1
+	->  feedback('d_aux: is OK\n',[])
+	;   feedback('d_aux: ~w is ~w + 1 NOT ok\n',[X,Y])
+	),
+	feedback('d_aux: X is ~w~n',[X]).
+d(0).
+
+e(X) :-
+	feedback('e_aux: before calling d(Y)'),
+	d(Y),
+	feedback('e_aux: after calling d(Y)'),
+	feedback('e_aux: Y is ~w~n',[Y]),
+	Y < 5,
+	feedback('e_aux: Y < 5 OK\n', []),
+	(   X is Y + 1
+	->  feedback('e_aux: is OK\n',[])
+	;   feedback('e_aux: ~w is ~w + 1 NOT ok\n',[X,Y])
+	),
+	feedback('e_aux: X is ~w~n',[X]).
+e(0).
+
+f(X) :- g(Y), Y < 5, X is Y + 1.
+f(0).
+
+g(X) :- f(Y), Y < 5, X is Y + 1.
+g(0).
+
+test(ex16) :-
+	once((d(_X), f(_Y))).
+
+:- end_tests(tabling_ex16).
+
+
+		 /*******************************
+		 *           EXAMPLE 17		*
+		 *******************************/
+
+:- begin_tests(tabling_ex17, [cleanup(abolish_all_tables)]).
+% Smaller version of example 10.
+
+% Expected answers
+
+expected_variants([d(_),e(_)]).
+expected_answers_for_variant(d(_),[d(0), d(2), d(5)]).
+expected_answers_for_variant(e(_),[e(1)]).
+
+:- table d/1, e/1.
+
+d(X) :- e(Y), Y < 2, X is Y + 1.  % Will never run, because e doesn't have any facts (initially)
+d(X) :- d(Y), Y < 2 , X is Y + 5. % Should run
+d(0).
+e(X) :- d(Y), Y < 2, X is Y + 1.
+% No facts for e
+
+test(ex17) :-
+	once(d(_)),
+	test_expected_variants_present,
+	test_answers_expected_tables.
+
+:- end_tests(tabling_ex17).
 
 
 		 /*******************************
